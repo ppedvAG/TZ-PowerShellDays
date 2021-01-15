24f3a4644c30383d46c68f39930db1515d41b9b1
Stefan Ober <StefanO@ppedv.de>
2021-01-15 11:23:26 +0100

    Behebung Tippfehler Dateiname

---
+++ SkriptExample/Get-LogonEvents.ps1
@@ -0,0 +1,7 @@
+﻿param(
+$EventId,
+$Newest,
+$Computername
+)
+
+Get-EventLog -LogName Security -ComputerName $Computername | Where-Object EventId -eq $EventId | Select-Object -First $Newest
--- SkriptExample/Get-LogonEvents.sp1.ps1
+++
@@ -1,2 +0,0 @@
-﻿
- Get-EventLog -LogName Security -ComputerName localhost | Where-Object EventId -eq 4624 | Select-Object -First 10
