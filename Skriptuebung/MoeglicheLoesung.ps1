344711e07579416c66e69627ec4919eab5e28384
Stefan Ober <StefanO@ppedv.de>
2021-01-18 11:45:50 +0100

    Function eingefügt zur Dateierstellung

--- Skriptuebung/MoeglicheLoesung.ps1
+++ Skriptuebung/MoeglicheLoesung.ps1
@@ -16,18 +16,41 @@ if($path.EndsWith("\") -eq $false)
 {
     $path = $path + "\"
 }
+
+#Funktionen
+function TestFilesCreation
+{
+    param(
+    [int]$Count,
+    [string]$path
+    )
+
+    #Prüfen ob Pfad mit \ endet, wenn nicht wird es hinzugefügt
+    if($path.EndsWith("\") -eq $false)
+    {
+        $path = $path + "\"
+    }
+
+    for($i = 1; $i -le $Count; $i++)
+    {
+        $FileNumber = "{0:D2}" -f $i
+        $Filepath = $path + "File" + $FileNumber + ".txt"
+        New-Item -Path $Filepath -ItemType File
+    }
+}
+
 $TestFilespath = $path + "TestFiles"
 
 if((Test-Path -Path $TestFilespath))
 {
     if($force)
-    {
-        Remove-Item -Path $TestFilespath -Recurse -Force
+    {   #Wenn Pfad vorhanden und Parameter Force angegeben, lösche Inhalt
+        Get-ChildItem -Path $TestFilespath -Recurse | Remove-Item -Force -Recurse
     }
     else
-    {
+    {   #Wenn Pfad vorhanden aber Force nicht angegeben, gebe Meldung aus und beende
         Write-Host -ForegroundColor Red -Object "Der Ordner Testfiles ist bereits vorhanden"
-        exit
+        exit #beendet die Skript Ausführung
     }
 }
 else
@@ -35,17 +58,17 @@ else
     New-Item -Path $TestFilespath -ItemType Directory
 }
 
+#Dateien im TestFiles Root Verzeichnis erstellen
+TestFilesCreation -Count $FileCount -path $TestFilespath
 
+#Schleife zum erstellen der Ordner
 for($i = 1; $i -le $DirCount;$i++)
 {
     $DirNumber = "{0:D2}" -f $i #Zahlenformatieren mit führenden Nullen z.B. 2 -> 02
     $dirpath = "$TestFilespath\Ordner$DirNumber"
-    New-Item -Path $dirpath -ItemType Directory
+    New-Item -Path $dirpath -ItemType Directory #erstellen der Ordner
 
-    for($j = 1; $j -le $FileCount; $j ++)
-    {
-        $FileNumber = "{0:D2}" -f $j
-        $filepath = "$dirpath\File$FileNumber.txt"
-        New-Item -Path $filepath -ItemType File
-    }
+    #Dateien in jeweiligen Unterordner erstellen
+    TestFilesCreation -Count $FileCount -path $dirpath
+
 }
