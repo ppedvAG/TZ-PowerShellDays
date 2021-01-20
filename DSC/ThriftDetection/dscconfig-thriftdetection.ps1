8ff2cf26afaa79af019db11e1b8d1f4af1527a16
Stefan Ober <StefanO@ppedv.de>
2021-01-20 11:29:17 +0100

    Beispiele für DSC

---
+++ DSC/ThriftDetection/dscconfig-thriftdetection.ps1
@@ -0,0 +1,28 @@
+﻿configuration WebServerConfiguration
+{
+    # Zum Abrufen der Knotenliste können Ausdrücke ausgewertet werden.
+    # Beispiel: $AllNodes.Where("Role -eq Web").NodeName
+    node ("Member1")
+    {
+        LocalConfigurationManager
+        {
+            #ApplyOnly: Nur einmalig Konfiguration anwenden
+            #ApplyAndMonitor: Konfiguration anwenden und bei Abweichung EventLog Eintrag
+            ConfigurationMode = "ApplyAndAutoCorrect" #Konfiguration anwenden und bei Abweichung automatisch wieder herrstellen        }
+        }
+        # Ressourcenanbieter aufrufen
+        # Beispiel: WindowsFeature, File
+        WindowsFeature WebServerRolle
+        {
+           Ensure = "Present"
+           Name   = "Web-Server"
+        }
+
+        WindowsFeature ManagementTools
+        {
+            Ensure = "Present"
+            Name = "Web-Mgmt-Tools"
+        }      
+    }
+}
+WebServerConfiguration -OutputPath "$(Get-Location)\Configuration"
---
+++ DSC/simple/simple.ps1
@@ -0,0 +1,22 @@
+﻿configuration WebServerConfiguration
+{
+    # Zum Abrufen der Knotenliste können Ausdrücke ausgewertet werden.
+    # Beispiel: $AllNodes.Where("Role -eq Web").NodeName
+    node ("Member1")
+    {
+        # Ressourcenanbieter aufrufen
+        # Beispiel: WindowsFeature, File
+        WindowsFeature WebServerRolle
+        {
+           Ensure = "Present"
+           Name   = "Web-Server"
+        }
+
+        WindowsFeature ManagementTools
+        {
+            Ensure = "Present"
+            Name = "Web-Mgmt-Tools"
+        }      
+    }
+}
+WebServerConfiguration -OutputPath (Get-Location)
