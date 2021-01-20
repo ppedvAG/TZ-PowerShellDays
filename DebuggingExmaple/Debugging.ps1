802fe4cd17544aa8a22d52bf7f6cf78f4aad75a4
Stefan Ober <StefanO@ppedv.de>
2021-01-20 09:06:17 +0100

    DebuggineExmaple

---
+++ DebuggingExmaple/Debugging.ps1
@@ -0,0 +1,21 @@
+﻿[cmdletBinding()]
+param(
+    [int]$Eingabe = 5
+)
+
+Write-Verbose -Message "ZusatzInfo1"
+
+Write-Debug -Message "Vor Datenabruf"
+Write-Host "Rufe Daten ab:" $Eingabe
+
+Write-Verbose -Message "ZusatzInfo2"
+
+Write-Debug -Message "Vor Datan Analyse"
+Write-Host "Analysiere Daten:" $Eingabe
+
+Write-Debug -Message "Vor Datenverarbeitung"
+Write-Host "Verarbeite Daten:" $Eingabe
+
+Write-Debug -Message "Vor Datenspeicherung"
+Write-Host "Speichere Änderungen:" $Eingabe
+
